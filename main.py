5b17ef98bba5ab5cdf6001e81180ecb8d930780f
Connor McLeod <z5058240@ad.unsw.edu.au>
2019-07-18 15:33:24 +1000

    Set up branch

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ main.py: Main file for program execution
 """
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 if __name__ == "__main__":
 
 	df_bodies = pd.read_csv("fakenews_dataset/train_bodies.csv")
 	df_stances = pd.read_csv("fakenews_dataset/train_stances.csv")
+
+	df = pd.merge(df_bodies, df_stances, on="Body ID")
+
+	# print column headings
+	print ("Dataframe column headings:\n", df.columns.values)
+
+	data_x = df[["articleBody","Headline"]].values
+	data_y = df["Stance"].values
+	train_x, valid_x, train_y, valid_y = train_test_split(data_x, data_y, test_size=0.20, random_state=0)
+
+
 
 	# to do:
 	# separate df_bodies and df_stances into train/validate/test
@@ -23,3 +35,4 @@ if __name__ == "__main__":
 		- Email Omar with our project and group members
 		- Ensure everyone understands github branches
 		- Ensure everyone understands virtual environment
+	"""
