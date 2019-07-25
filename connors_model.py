8ae5ac4d7b80ebf66a59f48f0153a7efe0d72aab
Connor McLeod <z5058240@ad.unsw.edu.au>
2019-07-25 20:03:44 +1000

    Rework scorer file to work as a function

--- connors_model.py
+++ connors_model.py
@@ -8,37 +8,39 @@ connors_model.py: Main calls on this file for model implementation
 import sys, csv
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from output.scorer_modified import *
 
 def write_to_csv(filename, heading, content):
 	"""
 	Function to write desired output to csv file
 	:param filename: the name of the file
-	:param heading: a Python list of the headers for the csv
-	:param content: a Python list of lists containing [headline, body ID, stance (prediction)]
-	:return: None
+	:param heading: a Python list of the headers for the csv ['Headline','Body ID','Stance']
+	:param content: a Python list of lists where each list has the same structure as the header
+	:return: created csv filepath
 	"""
-	with open(filename, mode='w') as test_file:
-		csv_writer = csv.writer(test_file)
+	with open(filename, mode='w') as file:
+		csv_writer = csv.writer(file)
 		csv_writer.writerow(heading)
 		for row in content:
 			csv_writer.writerow(row)
 	print (filename, " created")
+	return filename
 
 
 def connors_model():
 
-	#df_bodies = pd.read_csv("data/train_bodies.csv")
-	#df_stances = pd.read_csv("data/train_stances.csv")
+	# df_bodies = pd.read_csv("data/train_bodies.csv")
+	# df_stances = pd.read_csv("data/train_stances.csv")
 
 	# df = pd.merge(df_bodies, df_stances, on="Body ID")
-	# print column headings
+	# #print column headings
 	# print ("Dataframe column headings:\n", df.columns.values)
 
 	# data_x = df[["articleBody","Headline"]].values
 	# data_y = df["Stance"].values
 	# train_x, valid_x, train_y, valid_y = train_test_split(data_x, data_y, test_size=0.20, random_state=0)
 
-	# to do:
+	# # to do:
 	# learn from training subset using several learning frameworks (k NN, decision tree, etc)
 	# apply models to validation subset
 	# assess results of models on validation set
@@ -55,5 +57,9 @@ def connors_model():
 		['A Russian Guy Says His Justin Bieber Ringtone Saved Him From A Bear Attack','2','unrelated']
 	]
 
-	write_to_csv('output/cm_true_out.csv', heading, true_set)
-	write_to_csv('output/cm_test_out.csv', heading, test_set)
+	true_csv = write_to_csv('output/cm_true_out.csv', heading, true_set)
+	test_csv = write_to_csv('output/cm_test_out.csv', heading, test_set)
+
+	print("\nResults:\n")
+	report_score(true_csv, test_csv)
+
--- output/readme.md
+++ output/readme.md
@@ -9,5 +9,9 @@ The output csvs are then fed into 'scorer.py' using:
 
 `python scorer.py <test file> <model predictions file>`
 
-Each csv should have a 3-column header: Headline, Body ID, Stance
-Each following row is a headline, body ID and true or predicted stance
+Each csv should have a 3-column header: Headline, Body ID, Stance. Each following row is a headline, body ID and true or predicted stance
+
+# Scoring is as follows:
+`  +0.25 for each correct unrelated
+  +0.25 for each correct related (label is any of agree, disagree, discuss)
+  +0.75 for each correct agree, disagree, discuss`
