988ff36647c2c70a74d88761c306148e8d7d6c58
Connor McLeod <z5058240@ad.unsw.edu.au>
2019-07-25 19:06:23 +1000

    Setting up project folder for individual models

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 """
 COMP9417
 Assignment
+Authors: Connor McLeod (z5058240), name (zid), name (zid), name (zid)
 main.py: Main file for program execution
 """
 
+import sys, csv
 import pandas as pd
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
+from connors_model import *
+# import tensorflow as tf 				# import this to your own file if you use it
+
 
 
 if __name__ == "__main__":
-    df_bodies = pd.read_csv("data/train_bodies.csv")
-    df_stances = pd.read_csv("data/train_stances.csv")
-
-	df = pd.merge(df_bodies, df_stances, on="Body ID")
-
-	# print column headings
-	print ("Dataframe column headings:\n", df.columns.values)
-
-	data_x = df[["articleBody","Headline"]].values
-	data_y = df["Stance"].values
-	train_x, valid_x, train_y, valid_y = train_test_split(data_x, data_y, test_size=0.20, random_state=0)
 
 
+	connors_model(df_bodies, df_stances)
+	# your_model_goes_here
 
-	# to do:
-	# learn from training subset using several learning frameworks (k NN, decision tree, etc)
-	# apply models to validation subset
-	# assess results of models on validation set
+	print ("\nEnd of tests\n")
+
+
+	""" 
+	To do:
+	- Add your own model implementation to this mainfile by calling it from another file
+	- Document your research into your selected model for text classification
+	- ?
+	"""
+
+
---
+++ output/readme.md
@@ -0,0 +1,13 @@
+# 9417_MachineLearning
+
+Assignment - Fake news
+
+## Output
+
+The purpose of the output folder is to save your output csvs into.
+The output csvs are then fed into 'scorer.py' using:
+
+`python scorer.py <test file> <model predictions file>`
+
+Each csv should have a 3-column header: Headline, Body ID, Stance
+Each following row is a headline, body ID and true or predicted stance
