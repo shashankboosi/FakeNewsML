0b570aa26cc6aaa61f608fa3e588011dbf868225
shashankboosi <shashank1872@gmail.com>
2019-08-09 01:35:28 +1000

    Add data splitting to create train and validation data

---
+++ src/train_validation_split.py
@@ -0,0 +1,20 @@
+class DataSplit:
+
+    def __init__(self, ids, headline, body, split_size=0.8):
+        self.ids = ids
+        self.headline = headline
+        self.body = body
+        self.training_split_size = split_size
+
+    def split(self):
+        train_ids = self.ids[:int(self.training_split_size * len(self.ids))]
+        validation_ids = self.ids[int(self.training_split_size * len(self.ids)):]
+        train_stances = []
+        validation_stances = []
+        for stance in self.headline:
+            if int(stance['Body ID']) in train_ids:
+                train_stances.append(stance)
+            elif int(stance['Body ID']) in validation_ids:
+                validation_stances.append(stance)
+
+        return train_stances, validation_stances
