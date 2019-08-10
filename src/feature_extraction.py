5e92f566b326fb5f81ec6f94b6fc723a58fb2dcd
shashankboosi <shashank1872@gmail.com>
2019-08-10 18:05:57 +1000

    Add cosine similarity function to feature extraction

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -20,7 +20,10 @@ class Features:
             feature_division = len(intersection_of_sentences) / float(len(union_of_sentences))
             weight_features.append(feature_division)
 
-        return weight_features
+        return sp.coo_matrix(weight_features)
+
+    def cosine_sim(self, tfidf_weights):
+        return sp.coo_matrix(cosine_similarity(tfidf_weights, tfidf_weights))
 
     def tfidf_extraction(self, validation_headlines, validation_bodies, test_headlines, test_bodies):
         tf_headline_vectorizer = TfidfVectorizer(ngram_range=(1, 3), stop_words="english", lowercase=True,
